95cbb3c3f1ee74d640150d6e291bf6f1726b0b6c
Thrust <nepa.emanuele@gmail.com>
2021-05-27 19:31:22 +0200

    Completed project cleaning from old files and started building process of the game

---
+++ Assets/Scripts/SceneMng.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneMng : MonoBehaviour
+{
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
