94bbe07521b57bce7bff24f7b424087d0a30284e
Emanuele Nepa <nepaemanuele@outlook.it>
2022-08-21 18:05:45 +0200

    Build

--- Assets/_Scripts/Scriptables/ScriptablePlayer.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using UnityEngine;
-
-// Create a data container referred to the Player
-[CreateAssetMenu(fileName = "Player")]
-public class ScriptablePlayer : ScriptableUnitBase
-{
-    
-}
